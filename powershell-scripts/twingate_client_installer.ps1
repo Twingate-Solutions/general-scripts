2c73bd487f693364f03ddcfff7b94a78b0b7210d
bcameron-twingate <bcameron@twingate.com>
2024-07-15 11:31:25 -0700

    Major update and additions
    - Added a way to create a machinekey.conf file
    - Added a way to create a scheduled task to keep the client running
    - Added some additional checks around the uninstall code
    - Added bools to enable/disable some optional features
    - Added more instructions and better commenting
    - Tested locally

--- powershell-scripts/twingate_client_installer.ps1
+++ powershell-scripts/twingate_client_installer.ps1
@@ -1,21 +1,65 @@
-# This script is meant to be run as a scheduled task on a Windows machine
-# in order to initially install the Twingate Client and necessary .NET Desktop Runtime
-# as well as to update the Client if necessary.  It's not necessary to know if there's
-# a new version of the Client available, this can be scheduled to run on a monthly 
-# basis and will simply update to the most recent version.
+# This script is designed to install or update the Twingate Windows client application.
+# It can be run locally as a scheduled task, or pushed remotely via a tool like Intune.
 
-# If there's a concern about older versions of the Client still out in your deployed
-# machines then push this via your MDM solution and it will immediately update any
-# old versions.
+# The script has a couple of optional features:
+# - It can first uninstall the client app before re-installing it from scratch
+# - It can install a machinekey.conf to enforce always-on connectivity (mostly for Twingate Internet Security)
+# - It can create a scheduled task to auto-start the Twingate client application if it's ever quit by the user
 
-# Set the variables
+# By default the script will always check to see if the Twingate client application is running, and kill it.  It will
+# also check to see if the .NET Desktop Runtime 6.0 is installed, and install it if it is not.
+
+###################################
+##  Configure Optional Features  ##
+###################################
+
+# To uninstall the client app before re-installing, set to true
+$uninstallFirst = $false
+
+# To create a machinekey.conf file, set $createMachineKey to true, and paste the contents of the file in the $machineKey variable.
+# The machinekey.conf contents are found in your Twingate Admin Console, under the Internet Security section.
+# When you go to Client Configuration you can create a new machine key, and copy the contents to paste in the variable below.
+# Ex:
+# $machineKeyContent = @'
+# {
+#   "version": "2",
+#   "network": "test.twingate.com",
+#   "private_key": "-----BEGIN PRIVATE KEY-----\PRIVATEKEYGOESHERE\n-----END PRIVATE KEY-----",
+#   "id": "IDGOESHERE"
+# }
+# '@
+#
+# Make sure to paste the contents of the machinekey.conf file such that it matches the format above.
+
+$createMachineKey = $false
+$machineKeyTargetFolder = "C:\ProgramData\Twingate" # Don't touch this
+$machineKeyContent = @"
+machinekey
+"@
+
+# To create a scheduled task to auto-start the Twingate client application if it's ever quit by the user, set to true
+# You can also choose how often to check if the Twingate client is running, and how often to restart it.
+$createScheduledTask = $false
+$taskName = "Twingate Client Restart"
+$taskDescription = "This task will check every 5 minutes to see if the Twingate client is running, and restart it if it is not."
+$taskMinutes = 5
+
+###################################
+##         Set Variables         ##
+###################################
+
+# Twingate network name, ie networkname.twingate.com when you log in to the Admin Console
+# It's important to change this to your network name if you want to auto-populate it.
+# If you are installing a machinekey.conf then that will override this.
+$twingateNetworkName = "networkname" 
+
+# Path to the Twingate client executable post-installation
 $twingateClientPath = "C:\Program Files\Twingate\Twingate.exe"
 
-#############################
-##  Change the line below  ##
-#############################
-$twingateNetworkName = "networkname" #this is the name of the network in Twingate, ie networkname.twingate.com when you log in to the Admin Console
+# Twingate Windows service name
 $twingateServiceName = "twingate.service"
+
+
 
 # Check to see if Twingate is already running, if so kill it
 Write-Host [+] Checking for existing Twingate install
@@ -24,12 +68,16 @@ if ((Get-Process -Name "Twingate" -ErrorAction SilentlyContinue) -And (Get-Servi
 	Stop-Process -Name "Twingate" -Force -ErrorAction SilentlyContinue
 }
 
-# Un-comment the five lines below to do a fresh install each time, uninstall the client completely before re-installing
-# Write-Host [+] Uninstalling Twingate
-# $twingateApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name.Contains("Twingate")}
-# if ($twingateApp) {
-# 	$twingateApp.Uninstall()
-# }
+# If the uninstallFirst variable is set to true, then uninstall the Twingate client
+# This is useful if you want to ensure a clean install
+
+if ($uninstallFirst) {
+    Write-Host [+] Uninstall flag set, uninstalling Twingate Client application
+    $twingateApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name.Contains("Twingate")}
+    if ($twingateApp) {
+        $twingateApp.Uninstall()
+    }
+}
 
 # Check to see if the .NET Desktop Runtime 6.0 is already installed
 Write-Host [+] Checking if .NET Desktop Runtime 6.0 is already installed
@@ -57,4 +105,44 @@ Write-Host [+] Installing the Twingate Client
 cmd /c "msiexec.exe /i C:\Windows\Temp\TwingateInstaller.msi /qn network=$twingateNetworkName.twingate.com no_optional_updates=true"
 Write-Host [+] Finished installing Twingate Client
 Write-Host [+] Starting Twingate Client
-Start-Process -FilePath $twingateClientPath
+
+# If the createMachineKey variable is set to true, then create the machinekey.conf file
+if ($createMachineKey) {
+    Write-Host [+] Machinekey.conf flag set, creating machinekey.conf
+    Write-Host [+] Checking for target machinekey.conf folder
+    if (-not (Test-Path $machineKeyTargetFolder)) {
+        Write-Host [+] Creating Twingate folder
+        New-Item -ItemType Directory -Path $machineKeyTargetFolder
+    }
+    Write-Host [+] Creating machinekey.conf
+    New-Item "$machineKeyTargetFolder\machinekey.conf" -ItemType File -Value $machineKeyContent
+    Write-Host [+] Finished installing machinekey.conf
+}
+
+# If the createScheduledTask variable is set to true, then create the scheduled task
+if ($createScheduledTask) {
+    Write-Host [+] Scheduled Task flag set, creating scheduled task
+    Write-Host [+] Creating scheduled task
+    $action = New-ScheduledTaskAction -Execute $twingateClientPath
+    $taskTrigger = @(
+        $(New-ScheduledTaskTrigger -Once -At 12:01AM -RepetitionInterval (New-TimeSpan -Minutes $taskMinutes)),
+        $(New-ScheduledTaskTrigger -Daily -At 12:01AM),
+        $(New-ScheduledTaskTrigger -AtStartup)
+    )
+    $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+    Register-ScheduledTask -TaskName $taskName -Description $taskDescription -Action $action -Trigger $taskTrigger -Settings $taskSettings
+    Write-Host [+] Finished creating scheduled task
+
+    # Since a scheduled task has been created, start it and the Twingate service
+    Write-Host [+] Starting Task
+    Start-ScheduledTask -TaskName $taskName
+    Start-Service -Name $twingateServiceName -ErrorAction SilentlyContinue
+} else {
+    Write-Host [+] Scheduled Task flag not set, skipping scheduled task creation
+
+    # Start the Twingate service and application
+    Start-Process -FilePath $twingateClientPath
+    Start-Service -Name $twingateServiceName -ErrorAction SilentlyContinue
+}
+
+Write-Host [+] Finished running Twingate Client installer script
